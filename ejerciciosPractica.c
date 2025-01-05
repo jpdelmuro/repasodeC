399ca301e01197b001d5f27e19fb0b7cdd8ac360
Juan Pablo Del Muro <jpdelmuro@gmail.com>
2025-01-05 16:28:13 -0600

    Primera version, resuelto hasta problema 7

---
+++ ejerciciosPractica.c
@@ -0,0 +1,114 @@
+/*
+//Problema 1
+#include <stdio.h>
+int main(){
+	int n1;
+	int n2;
+	printf("Introduce el numero 1: \n");
+	scanf("%d",&n1);
+	printf("Introduce el numero 2: \n");
+	scanf("%d",&n2);
+	printf("La suma es: %d",n1+n2);
+	return 0;
+}
+*/
+/*
+// Problema 2
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float area;
+	float radio;
+	printf("Introduce el radio de tu circulo: ");
+	scanf("%f",&radio);
+	area = M_PI * pow(radio , 2);
+	printf("El area es %.2f ", area);
+	
+	return 0;
+}
+*/
+// Problema 3
+/*
+#include <stdio.h>
+int main(){
+	float cel;
+	printf("Introduce la cantidad de grados celsius: ");
+	scanf("%f",&cel);
+	float fah = ((9.0/5)*(cel)+32);
+	printf("el resultado son %.2f grados Fahrenheit",fah);
+	
+	
+	return 0;
+}
+*/
+// Problema 4
+/*
+#include <stdio.h>
+int main(){
+	int n;
+	printf("introduce el numero que quieres saber si es par o impar: ");
+	scanf("%d",&n);
+	if (n % 2 == 0)
+		printf("el numero es par");
+	else
+		printf("el numero es impar");
+	return 0;
+}
+*/
+// Problema 5
+/*
+#include <stdio.h>
+int main(){
+	int n,i;
+	int fact = 1;
+	scanf("%d",&n);
+	for(i = 1; i <= n; i++){
+		fact = fact * i;
+	}
+	printf("%d",fact);
+	return 0;
+}
+*/
+// Problema 6
+/*
+#include <stdio.h>
+int main(){
+	int i,n;
+	printf("introduce el numero del que te gustaria la tabla: ");
+	scanf("%d",&n);
+	for(i=1;i<=10;i++){
+		printf("%d x %d = %d\n",n,i,n*i);
+	}
+	
+	return 0;
+}
+
+*/
+// Problema 7
+#include <stdio.h>
+int main(){
+	int n,i,cont=0;
+	printf("Introduce un numero para saber si es primo: "); scanf("%d",&n);
+	for(i = 1; i <= n; i++){
+		if(n % i == 0){
+			cont ++;
+		}
+	}
+	if (cont > 2)
+		printf("%d no es primo",n);
+	else
+		printf("%d es primo",n);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
